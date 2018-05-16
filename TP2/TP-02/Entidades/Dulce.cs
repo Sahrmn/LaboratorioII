f7a49b51e0ebd1eb8badd65e21895804230e5e43
Sahrmn <samy32m@gmail.com>
2018-05-16 18:52:10 -0300

    Agregado TP2

---
+++ TP2/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codigo, ConsoleColor color):base(codigo, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : "+ this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+            //string retValue = "DULCE\n";
+            //retValue += base.Mostrar();
+            //retValue += "CALORIAS :" + this.CantidadCalorias + "\n\n";
+            //retValue += "---------------------";
+            //return retValue;
+        }
+    }
+}
---
+++ TP2/TP-02/Entidades/Producto.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    /// <summary>
+    /// La clase Producto será abstracta, evitando que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+        private EMarca _marca;
+        private string _codigoDeBarras;
+        private ConsoleColor _colorPrimarioEmpaque;
+
+        public Producto(string codigoDeBarras, EMarca marca, ConsoleColor color)
+        {
+            this._codigoDeBarras = codigoDeBarras;
+            this._marca = marca;
+            this._colorPrimarioEmpaque = color;
+        }
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+        protected abstract short CantidadCalorias
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            //string retValue = "CODIGO DE BARRAS: " + this._codigoDeBarras + "\n";
+            //retValue += "MARCA: " + this._marca + "\n";
+            //retValue += "COLOR EMPAQUE: " + this._colorPrimarioEmpaque + "\n";
+            //retValue += "---------------------";
+            //return retValue;
+            return (string)this;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CODIGO DE BARRAS: " + p._codigoDeBarras + "\r");
+            sb.AppendLine("MARCA          : " + p._marca.ToString() + "\r");
+            sb.AppendLine("COLOR EMPAQUE  : " + p._colorPrimarioEmpaque.ToString() + "\r");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+
+            //return p.Mostrar();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+    }
+}
